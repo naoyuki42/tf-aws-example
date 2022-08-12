fa11218f7ca299528bc7e1102ba96e711f8ffc0a
naoyuki42 <naoyuki.7042@outlook.jp>
2022-08-12 23:51:23 +0900

    add: IAMロール

---
+++ iam_role/main.tf
@@ -0,0 +1,29 @@
+variable "name" {}
+variable "policy" {}
+variable "identifier" {}
+
+resource "aws_iam_role" "default" {
+  name               = var.name
+  assume_role_policy = data.aws_iam_policy_document.assume_role.json
+}
+
+data "aws_iam_policy_document" "assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = [var.identifier]
+    }
+  }
+}
+
+resource "aws_iam_policy" "default" {
+  name   = "default"
+  policy = var.policy
+}
+
+resource "aws_iam_role_policy_attachment" "example" {
+  role       = aws_iam_role.default.name
+  policy_arn = aws_iam_policy.default.arn
+}
