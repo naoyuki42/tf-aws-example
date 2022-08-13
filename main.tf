823051a1666649c39596cbc19c4a41c3159eb526
naoyuki42 <naoyuki.7042@outlook.jp>
2022-08-13 18:02:47 +0900

    add: セキュリティグループ

--- main.tf
+++ main.tf
@@ -17,3 +17,12 @@ data "aws_iam_policy_document" "allow_describe_regions" {
     resources = ["*"]
   }
 }
+
+# セキュリティグループモジュール
+module "default_sg" {
+  source      = "./security_group"
+  name        = "module_sg"
+  vpc_id      = aws_vpc.default.id
+  port        = 80
+  cidr_blocks = ["0.0.0.0/0"]
+}
