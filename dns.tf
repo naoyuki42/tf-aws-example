e44b579214a785bb183a3adfc2edbac13d10d84c
naoyuki42 <naoyuki.7042@outlook.jp>
2022-08-14 16:23:06 +0900

    add: certicitate

--- dns.tf
+++ dns.tf
@@ -2,10 +2,6 @@ data "aws_route53_zone" "default" {
   name = "nao42.com"
 }
 
-resource "aws_route53_zone" "test_default" {
-  name = "test.nao42.com"
-}
-
 resource "aws_route53_record" "default" {
   zone_id = data.aws_route53_zone.default.id
   name    = data.aws_route53_zone.default.name
@@ -20,4 +16,27 @@ resource "aws_route53_record" "default" {
 
 output "domain_name" {
   value = aws_route53_record.default.name
+}
+
+resource "aws_acm_certificate" "default" {
+  domain_name               = aws_route53_record.default.name
+  subject_alternative_names = []
+  validation_method         = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_route53_record" "default_certificate" {
+  name    = tolist(aws_acm_certificate.default.domain_validation_options)[0].resource_record_name
+  type    = tolist(aws_acm_certificate.default.domain_validation_options)[0].resource_record_type
+  records = [tolist(aws_acm_certificate.default.domain_validation_options)[0].resource_record_value]
+  zone_id = data.aws_route53_zone.default.id
+  ttl     = 60
+}
+
+resource "aws_acm_certificate_validation" "default" {
+  certificate_arn         = aws_acm_certificate.default.arn
+  validation_record_fqdns = [aws_route53_record.default_certificate.fqdn]
 }
