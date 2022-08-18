2cfa29c96182479bca75a6e22311b20a2c9a87b8
naoyuki42 <naoyuki.7042@outlook.jp>
2022-08-18 19:06:00 +0900

    add: ssm instance

--- codePipeline.tf
+++ codePipeline.tf
@@ -1,102 +1,102 @@
-resource "aws_codepipeline" "default" {
-  name     = "default"
-  role_arn = module.codepipeline_role.iam_role_arn
+# resource "aws_codepipeline" "default" {
+#   name     = "default"
+#   role_arn = module.codepipeline_role.iam_role_arn
 
-  stage {
-    name = "Source"
+#   stage {
+#     name = "Source"
 
-    action {
-      name             = "Source"
-      category         = "Source"
-      owner            = "ThirdParty"
-      provider         = "GitHub"
-      version          = 1
-      output_artifacts = ["Source"]
+#     action {
+#       name             = "Source"
+#       category         = "Source"
+#       owner            = "ThirdParty"
+#       provider         = "GitHub"
+#       version          = 1
+#       output_artifacts = ["Source"]
 
-      configuration = {
-        Owner                = "your-github-name"
-        Repo                 = "your-repository"
-        Branch               = "master"
-        PollForSourceChanges = false
-      }
-    }
-  }
+#       configuration = {
+#         Owner                = "your-github-name"
+#         Repo                 = "your-repository"
+#         Branch               = "master"
+#         PollForSourceChanges = false
+#       }
+#     }
+#   }
 
-  stage {
-    name = "Build"
+#   stage {
+#     name = "Build"
 
-    action {
-      name             = "Build"
-      category         = "Build"
-      owner            = "AWS"
-      provider         = "CodeBuild"
-      version          = 1
-      input_artifacts  = ["Source"]
-      output_artifacts = ["Build"]
+#     action {
+#       name             = "Build"
+#       category         = "Build"
+#       owner            = "AWS"
+#       provider         = "CodeBuild"
+#       version          = 1
+#       input_artifacts  = ["Source"]
+#       output_artifacts = ["Build"]
 
-      configuration = {
-        ProjectName = aws_codebuild_project.default.id
-      }
-    }
-  }
+#       configuration = {
+#         ProjectName = aws_codebuild_project.default.id
+#       }
+#     }
+#   }
 
-  stage {
-    name = "Deploy"
+#   stage {
+#     name = "Deploy"
 
-    action {
-      name            = "Deploy"
-      category        = "Deploy"
-      owner           = "AWS"
-      provider        = "ECS"
-      version         = 1
-      input_artifacts = ["Build"]
+#     action {
+#       name            = "Deploy"
+#       category        = "Deploy"
+#       owner           = "AWS"
+#       provider        = "ECS"
+#       version         = 1
+#       input_artifacts = ["Build"]
 
-      configuration = {
-        ClusterName = aws_ecs_cluster.default.name
-        ServiceName = aws_ecs_service.default.name
-        FileName    = "imagedefinitions.json"
-      }
-    }
-  }
+#       configuration = {
+#         ClusterName = aws_ecs_cluster.default.name
+#         ServiceName = aws_ecs_service.default.name
+#         FileName    = "imagedefinitions.json"
+#       }
+#     }
+#   }
 
-  artifact_store {
-    location = aws_s3_bucket.artifact.id
-    type     = "S3"
-  }
-}
+#   artifact_store {
+#     location = aws_s3_bucket.artifact.id
+#     type     = "S3"
+#   }
+# }
 
-resource "aws_codepipeline_webhook" "default" {
-  name            = "default"
-  target_pipeline = aws_codepipeline.default.name
-  target_action   = "Scource"
-  authentication  = "GITHUB_HMAC"
+# resource "aws_codepipeline_webhook" "default" {
+#   name            = "default"
+#   target_pipeline = aws_codepipeline.default.name
+#   target_action   = "Scource"
+#   authentication  = "GITHUB_HMAC"
 
-  authentication_configuration {
-    secret_token = "VeryRandomStringMoreThan20Byte!"
-  }
+#   authentication_configuration {
+#     secret_token = "VeryRandomStringMoreThan20Byte!"
+#   }
 
-  filter {
-    json_path    = "$.ref"
-    match_equals = "refs/heads/{Branch}"
-  }
-}
+#   filter {
+#     json_path    = "$.ref"
+#     match_equals = "refs/heads/{Branch}"
+#   }
+# }
 
-provider "github" {
-  organization = "your-github-name"
-}
+# provider "github" {
+#   organization = "your-github-name"
+# }
 
-resource "github_repository_webhook" "default" {
-  repository = "your-repository"
+# resource "github_repository_webhook" "default" {
+#   repository = "your-repository"
 
-  configuration {
-    url          = aws_codepipeline_webhook.default.url
-    secret       = "VeryRandomStringMoreThan20Byte!"
-    content_type = "json"
-    insecure_ssl = false
-  }
+#   configuration {
+#     url          = aws_codepipeline_webhook.default.url
+#     secret       = "VeryRandomStringMoreThan20Byte!"
+#     content_type = "json"
+#     insecure_ssl = false
+#   }
 
-  events = ["push"]
-}
+#   events = ["push"]
+# }
 
 resource "aws_s3_bucket" "artifact" {
   bucket = "artifact--20220818"
--- ssm.tf
+++ ssm.tf
@@ -15,3 +15,54 @@ resource "aws_ssm_parameter" "db_password" {
     ignore_changes = [value]
   }
 }
+
+resource "aws_instance" "default_for_operation" {
+  ami                  = "ami-0c3fd0f5d33134a76"
+  instance_type        = "t3.micro"
+  iam_instance_profile = aws_iam_instance_profile.ec2_for_ssm.name
+  subnet_id            = aws_subnet.private_01.id
+  user_data            = file("./user_data.sh")
+}
+
+resource "aws_iam_instance_profile" "ec2_for_ssm" {
+  name = "ec2-for-ssm"
+  role = module.ec2_for_ssm_role.iam_role_name
+}
+
+module "ec2_for_ssm_role" {
+  source     = "./iam_role"
+  name       = "ec2-for-ssm"
+  identifier = "ec2.amazonaws.com"
+  policy     = data.aws_iam_policy_document.ec2_for_ssm.json
+}
+
+data "aws_iam_policy_document" "ec2_for_ssm" {
+  source_json = data.aws_iam_policy.ec2_for_ssm.policy
+
+  statement {
+    effect    = "Allow"
+    resources = ["*"]
+
+    actions = [
+      "s3:PutObject",
+      "logs:PutLogEvents",
+      "logs:CreateLogStream",
+      "ecr:GetAuthorizationToken",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:GetDownloadUrlForLayer",
+      "ecr:BatchGetImage",
+      "ssm:GetParameter",
+      "ssm:GetParameters",
+      "ssm:GetParametersByPath",
+      "kms:Decrypt",
+    ]
+  }
+}
+
+data "aws_iam_policy" "ec2_for_ssm" {
+  arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+output "operation_instance_id" {
+  value = aws_instance.default_for_operation.id
+}
